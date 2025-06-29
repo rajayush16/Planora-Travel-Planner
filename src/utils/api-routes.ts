fb5d51508f181d288223f6f5cf5703b2d523f6c1
rajayush16 <ayush.raj12516@gmail.com>
2025-06-30 03:38:22 +0530

    Integrated Redis and modified Admin Page

--- src/utils/api-routes.ts
+++ src/utils/api-routes.ts
@@ -3,3 +3,22 @@ export const ADMIN_API_ROUTES = {
   CREATE_JOB: "/admin/createJob",
   JOB_DETAILS: "/admin/job-details",
 };
+
+export const USER_API_ROUTES = {
+  SIGNUP: "/auth/signup",
+  LOGIN: "/auth/login",
+  ME: "/auth/me",
+  TRIPDATA: "/trips",
+  FLIGHT_SCRAPE: "/flights/scrape",
+  FLIGHT_SCRAPE_STATUS: "/flights/scrape-status",
+  HOTELS_SCRAPE: "/hotels/scrape",
+  HOTELS_SCRAPE_STATUS: "/hotels/scrape-status",
+  GET_ALL_TRIPS: "/all-trips",
+  GET_ALL_HOTELS: "/all-hotels",
+  GET_UNIQUE_TRIP_CITIES: "/home/unique-cities",
+  GET_CITY_HOTELS: "/home/city-hotels",
+  GET_CITY_TRIPS: "/city-trips",
+  CREATE_BOOKING: "/booking",
+  GET_USER_BOOKINGS: "/booking/get-bookings",
+  GET_ALL_BOOKINGS: "/booking",
+};
