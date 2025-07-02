59b280bd480a42cdfe06f69137e36036d2dccf6e
rajayush16 <ayush.raj12516@gmail.com>
2025-07-03 03:28:14 +0530

    Home functionality

--- src/app/app-protector.ts
+++ src/app/app-protector.ts
@@ -1,8 +1,9 @@
 "use client";
 
-import { apiClient } from "@/lib";
+// import { apiClient } from "@/lib";
 import { useAppStore } from "@/store";
 import { USER_API_ROUTES } from "@/utils/api-routes";
+import axios from "axios";
 
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -14,7 +15,7 @@ const AppProtector = () => {
   useEffect(() => {
     if (!userInfo) {
       const getUserInfo = async () => {
-        const response = await apiClient.get(USER_API_ROUTES.ME);
+        const response = await axios.get(USER_API_ROUTES.ME);
         if (response.data.userInfo) {
           setUserInfo(response.data.userInfo);
         }
